83cd6b6d16eef698697d64a5738155a03b58807f
jsiwek <jsiwek@pc14i.labs.wmi.amu.edu.pl>
2018-11-05 15:52:23 +0100

    initial

---
+++ Classes5trueCBE/src/main/java/pl/edu/amu/dino/classes5truecbe/Substraction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dino.classes5truecbe;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class Substraction {
+    public static String (String a, String b){
+    return "0";}
+}
