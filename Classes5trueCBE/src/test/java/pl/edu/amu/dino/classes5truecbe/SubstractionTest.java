ce0b5584e37029ce24ef25174f6f186cf8b2c7f4
jsiwek <jsiwek@pc14i.labs.wmi.amu.edu.pl>
2018-11-05 15:52:08 +0100

    initial

---
+++ Classes5trueCBE/src/test/java/pl/edu/amu/dino/classes5truecbe/SubstractionTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.dino.classes5truecbe;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jsiwek
+ */
+public class SubstractionTest {
+    
+    public SubstractionTest() {
+    }
+
+    @Test
+    public void testString() {
+        System.out.println("String");
+        String a = "1";
+        String b = "2";
+        Substraction.String(a, b);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
